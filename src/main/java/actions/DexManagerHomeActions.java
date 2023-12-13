fd2ef983dcfdeb9522d616d1cc36474ffe39e694
Gabriel Sirota <gsirota@frba.utn.edu.ar>
2023-12-13 18:07:05 -0300

    Corrijo validacion login y chromedriver

--- src/main/java/actions/DexManagerHomeActions.java
+++ src/main/java/actions/DexManagerHomeActions.java
@@ -1,14 +1,24 @@
 package actions;
 
+import com.codeborne.selenide.Configuration;
+import io.cucumber.java.BeforeAll;
 import io.cucumber.java.en.*;
+import org.openqa.selenium.chrome.ChromeOptions;
+
 import org.testng.Assert;
 
-import java.io.IOException;
 import static com.codeborne.selenide.Selenide.*;
 import static pages.HomePage.*;
 import static utils.Props.getPropertie;
 
 public class DexManagerHomeActions {
+
+    @BeforeAll
+    public static void setup() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments( "--disable-gpu", "--window-size=1920,1200","--ignore-certificate-errors","--disable-extensions","--no-sandbox","--disable-dev-shm-usage");
+        Configuration.browserCapabilities.setCapability(ChromeOptions.CAPABILITY, options);
+    }
     @Given("el sitio de DexManager")
     public void visitDexManager(){
         String url = getPropertie("dexManager","URL");
@@ -28,7 +38,7 @@ public class DexManagerHomeActions {
     @Then("se valida la pagina de inicio")
     public void validateHomePage(){
 
-        Assert.assertEquals(challengeQALogo().getText(),"CHALLENGE QA");
+        Assert.assertEquals(challengeQALogo().getText(),"ARG - CHALLENGE QA");
         closeWebDriver();
     }
 }
